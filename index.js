ad0fc113783cc6fe5dcd3156898de884c2b3d962
Adam Bradbury <f.adambrad@gmail.com>
2017-10-04 09:44:32 -0600

    finished html wireframe

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const endpoint = "https://www.googleapis.com/youtube/v3/search"
+
+
+function getInfo () {};
+function renderResults () {
+    `<div>eventual html to be rendered</div>`;
+};
+function displayResults () {};
+
+
 console.log('hello');
 alert('working?');
